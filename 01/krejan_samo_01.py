37c83587abd174300acf2dd920cb3c373c202663
hyh <samo.krejan@gmail.com>
2025-10-10 21:43:58 +0200

    fix: funcs work now

--- 01/krejan_samo_01.py
+++ 01/krejan_samo_01.py
@@ -2,68 +2,69 @@
 import numpy as np
 import mpmath
 from mpmath import mp
+import matplotlib.pyplot as plt
 
 
 # constants and presets
 
-mp.dps = 60
+mp.dps = 500
 alpha = mp.mpf('0.355028053887817239')
 beta = mp.mpf('0.258819403792806798')
 
 
 # funcs
-# tudi vsi int-i so v mp.mpf saj mi je nekaj delalo tezave z natancnostjo in jih kasneje nisem spremenil nazaj. na performance ne vplivajo skoraj nic
 
-def f(x: mp.mpf, n: int) -> mp.mpf:
+
+def f(x, n):
     out = mp.mpf('1')
     term = mp.mpf('1')
     for i in range(1, n + 1):
-        term *= mp.mpf(x)**mp.mpf('3') / (mp.mpf('3')*mp.mpf(i) * (mp.mpf('3')*mp.mpf(i) - mp.mpf('1')))
+        term *= mp.mpf(x)**3 / (3*i * (3*i - 1))
         out += term
     return out
 
 
-def g(x: mp.mpf, n: int) -> mp.mpf:
+def g(x, n):
     out = mp.mpf(x)
     term = mp.mpf(x)
     for i in range(1, n + 1):
-        term *= mp.mpf(x)**mp.mpf('3') / (mp.mpf('3')*mp.mpf(i) * (mp.mpf('3')*mp.mpf(i) + mp.mpf('1')))
+        term *= mp.mpf(x)**3 / (3*i * (3*i + 1))
         out += term
     return out
 
 
-def L(x: mp.mpf, n: int=100) -> mp.mpf:
+def L(x, n):
     out = mp.mpf('1')
     term = mp.mpf('1')
     for i in range(1, n + 1):
-        multiply = (mp.mpf(3)*mp.mpf(i) + mp.mpf('5/2')) * (mp.mpf(3)*mp.mpf(i) + mp.mpf('1/2')) / ((mp.mpf(i) + mp.mpf(1)) * mp.mpf(18) * mp.mpf(x))
-        if np.abs(multiply) > 1:
+        new = term * (3 * i - mp.mpf('5/2')) * (3 * i - mp.mpf('1/2')) / (18 * x * i)
+        if np.abs(new) > np.abs(term):
             return out, i - 1
-        term *= multiply
+        term = new
         out += term
     return out, n
 
 
-def P(x: mp.mpf, n: int=100) -> mp.mpf:
+def P(x, n):
     out = mp.mpf('1')
     term = mp.mpf('1')
     for i in range(1, n + 1):
-        multiply = - (mp.mpf(6)*mp.mpf(i) - mp.mpf('5/2')) * (mp.mpf(3)*mp.mpf(i) - mp.mpf('1/2')) * (mp.mpf(6)*mp.mpf(i) - mp.mpf('7/2')) * (mp.mpf(3)*mp.mpf(i) - mp.mpf('11/2')) / ((mp.mpf(2)*mp.mpf(i) - mp.mpf(1)) * mp.mpf(2)*mp.mpf(i) * mp.mpf(18)**mp.mpf(2) * mp.mpf(x)**mp.mpf(2))
-        if np.abs(multiply) > 1:
+        new = term * (6 * i - mp.mpf('1/2')) * (6 * i - mp.mpf('5/2')) * (6 * i - mp.mpf('7/2')) * (6 * i - mp.mpf('11/2')) / (mp.mpf(-324) * x**2 * 2 * i * (2 * i - 1))
+        if np.abs(new) > np.abs(term):
             return out, i - 1
-        term *= multiply
+        term = new
         out += term
     return out, n
 
 
-def Q(x: mp.mpf, n: int=100) -> mp.mpf:
+def Q(x, n):
     out = mp.mpf('5/72') / mp.mpf(x)
-    term = mp.mpf('5/72') / mp.mpf(x)
+    term = out
     for i in range(1, n + 1):
-        multiply = - (mp.mpf(6)*mp.mpf(i) - mp.mpf('5/2')) * (mp.mpf(3)*mp.mpf(i) - mp.mpf('1/2')) * (mp.mpf(6)*mp.mpf(i) + mp.mpf('5/2')) * (mp.mpf(3)*mp.mpf(i) + mp.mpf('1/2')) / ((mp.mpf(2)*mp.mpf(i) + mp.mpf(1)) * mp.mpf(2)*mp.mpf(i) * mp.mpf(18)**mp.mpf(2) * mp.mpf(x)**mp.mpf(2))
-        if np.abs(multiply) > 1:
+        new = term * (6 * i - mp.mpf('1/2')) * (6 * i - mp.mpf('5/2')) * (6 * i + mp.mpf('1/2')) * (6 * i + mp.mpf('5/2')) / (mp.mpf(-324) * x**2 * 2 * i * (2 * i + 1))
+        if np.abs(new) > np.abs(term):
             return out, i - 1
-        term *= multiply
+        term = new
         out += term
     return out, n
 
@@ -73,16 +74,18 @@ def Ai_mac(x, n):
 
 
 def Ai_neg(x, n):
-    ksi = mp.mpf('2/3') * mpmath.fabs(x) ** (mp.mpf('3/2'))
-    q = Q(x, n)
-    p = P(x, n)
-    return mpmath.re((mp.mpf(1) / (mp.sqrt(mp.pi) * (- mp.mpf(x)) ** (mp.mpf('1/4')))) * (mp.sin(ksi - (mp.pi / mp.mpf(4))) * q[0] + mp.cos(ksi - (mp.pi / mp.mpf(4))) * p[0])), max(p[1], q[1])
+    ksi = mp.mpf('2/3') * mp.sqrt(mpmath.fabs(x) ** (mp.mpf('3')))
+    q = Q(ksi, n)
+    p = P(ksi, n)
+    T = mp.mpf(1) / (mp.sqrt(mp.pi) * mp.sqrt(mp.sqrt(-1*x)))
+    fi = mp.pi / mp.mpf(4)
+    return mpmath.re(T * (mp.sin(ksi - fi) * q[0] + mp.cos(ksi - fi) * p[0])), max(p[1], q[1])
 
 
 def Ai_pos(x, n):
-    ksi = mp.mpf('2/3') * mpmath.fabs(x) ** (mp.mpf('3/2'))
+    ksi = mp.mpf('2/3') * mp.sqrt(mpmath.fabs(x) ** (mp.mpf('3')))
     l = L(- ksi, n)
-    return mp.exp(- ksi) * l[0] / (mp.mpf(2) * mp.sqrt(mp.pi) * mp.mpf(x) ** mp.mpf('1/4')), l[1]
+    return mp.exp(- ksi) * l[0] / (mp.mpf(2) * mp.sqrt(mp.pi) * mp.sqrt(mp.sqrt(x))), l[1]
 
 
 def Bi_mac(x, n):
@@ -90,22 +93,42 @@ def Bi_mac(x, n):
 
 
 def Bi_neg(x, n):
-    ksi = mp.mpf('2/3') * mpmath.fabs(x) ** (mp.mpf('3/2'))
-    q = Q(x, n)
-    p = P(x, n)
-    return mpmath.re((mp.mpf(1) / (mp.sqrt(mp.pi) * (- mp.mpf(x)) ** (mp.mpf('1/4')))) * (- mp.sin(ksi - (mp.pi / mp.mpf(4))) * p[0] + mp.cos(ksi - (mp.pi / mp.mpf(4))) * q[0])), max(p[1], q[1])
+    ksi = mp.mpf('2/3') * mp.sqrt(mpmath.fabs(x) ** (mp.mpf('3')))
+    q = Q(ksi, n)
+    p = P(ksi, n)
+    T = mp.mpf(1) / (mp.sqrt(mp.pi) * mp.sqrt(mp.sqrt(-1*x)))
+    fi = mp.pi / mp.mpf(4)
+    return mpmath.re(T * (mp.cos(ksi - fi) * q[0] - mp.sin(ksi - fi) * p[0])), max(p[1], q[1])
 
 
 def Bi_pos(x, n):
-    ksi = mp.mpf('2/3') * mpmath.fabs(x) ** (mp.mpf('3/2'))
+    ksi = mp.mpf('2/3') * mp.sqrt(mpmath.fabs(x) ** (mp.mpf('3')))
     l = L(ksi, n)
-    return mp.exp(ksi) * l[0] / (mp.mpf(2) * mp.sqrt(mp.pi) * mp.mpf(x) ** mp.mpf('1/4')), l[1]
+    return mp.exp(ksi) * l[0] / (mp.sqrt(mp.pi) * mp.sqrt(mp.sqrt(x))), l[1]
 
-
-
-
-
-print(Bi_mac(30, 100), mp.airybi(30))
-# print(mp.mpf(3))
 
 # graphs
+
+x = np.arange(-30, -10, 0.1)
+
+y = [Ai_neg(i, 100)[0] for i in x]
+
+y1 = [float(i) for i in y]
+y2 = [float(mp.airyai(i)) for i in x]
+
+y3 = np.abs([float(mp.airyai(i) - Ai_neg(mp.mpf(i), 100)[0]) for i in x])
+
+# y4 = [float(Ai_neg(mp.mpf(i), 1000)[1]) for i in x]
+
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+# plt.yscale('log')
+plt.show()
+plt.plot(x, y3)
+plt.yscale('log')
+plt.show()
+# plt.plot(x, y4)
+# plt.yscale('log')
+# plt.show()
+
